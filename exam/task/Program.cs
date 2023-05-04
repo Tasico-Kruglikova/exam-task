9db386bb63c0c3cbb0fae08e5335aafc6e755ce4
Tasico Kruglikova <prosto-lo@yandex.ru>
2023-05-04 16:02:20 +0500

    finished task

--- exam/task/Program.cs
+++ exam/task/Program.cs
@@ -1,4 +1,4 @@
-﻿string[] array1 = new string[5] { "123", "23", "hello", "world", "res" };
+﻿string[] array1 = new string[4] { "world", "963", "hello", "res" };
 string[] array2 = new string[array1.Length];
 int count = 0;
 void SecondArrayWithIF(string[] array1, string[] array2)
